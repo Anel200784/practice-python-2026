13d18f3e701f52efd9a52f01714532b9e6ab3ccb
Anel200784 <anellabitai@gmail.com>
2026-05-29 15:54:51 +0500

    Add files via upload

---
+++ Jup_taq.py
@@ -0,0 +1,6 @@
+number = int(input("Сан енгіз: "))
+
+if number % 2 == 0:
+    print("Жұп")
+else:
+    print("Тақ")
---
+++ string.py
@@ -0,0 +1,9 @@
+name = "Anel"
+age = 18
+height = 1.65
+student = True
+
+print(name)
+print(age)
+print(height)
+print(student)
